c61b81b56b2dc7a1fb7643a6e61d66029d6e3af6
roostAI <demo@roost.ai>
2024-11-16 03:30:26 +0000

    Integration test generated by RoostGPT
    Using AI Model gpt-4o-standard

---
+++ src/test/java/org/springframework/api_tests/CredentialRegisterSwaggerServerSwaggerSwaggerTest.java
@@ -0,0 +1,23 @@
+
+    package org.springframework.api_tests;
+  
+    import com.intuit.karate.Results;
+    import com.intuit.karate.Runner;
+    // import com.intuit.karate.http.HttpServer;
+    // import com.intuit.karate.http.ServerConfig;
+    import org.junit.jupiter.api.Test;
+  
+    import static org.junit.jupiter.api.Assertions.assertEquals;
+  
+    class CredentialRegisterSwaggerServerSwaggerSwaggerTest {
+  
+        @Test
+        void testAll() {
+            String swagger_184f1d2b61_url = System.getenv().getOrDefault("SWAGGER_184F1D2B61_URL", "http://127.0.0.1:4010");
+            Results results = Runner.path("src/test/java/org/springframework/api_tests/CredentialRegisterSwaggerServerSwaggerSwagger")
+                    .systemProperty("SWAGGER_184F1D2B61_URL",swagger_184f1d2b61_url)
+                    .reportDir("testReport").parallel(1);
+            assertEquals(0, results.getFailCount(), results.getErrorMessages());
+        }
+  
+    }
