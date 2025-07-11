1c14f02d557eff09141b9a53e33642a320d614c2
roostAI <108646238+roostAI@users.noreply.github.com>
2025-07-11 06:54:42 +0000

    Add API Tests (Karate Framework, Azure Open AI) generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ src/test/java/org/springframework/api_tests/RoostIoSampleApiRefsHeadsMainApisAddressValidationMqas100Swagger/RoostIoSampleApiRefsHeadsMainApisAddressValidationMqas100SwaggerTest.java
@@ -0,0 +1,27 @@
+
+package org.springframework.api_tests.RoostIoSampleApiRefsHeadsMainApisAddressValidationMqas100Swagger;
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+// import com.intuit.karate.http.HttpServer;
+// import com.intuit.karate.http.ServerConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RoostIoSampleApiRefsHeadsMainApisAddressValidationMqas100SwaggerTest {
+
+	@Test
+	void testAll() {
+		String APIHOST1 = System.getenv().get("API_HOST1");
+		String AUTHTOKEN11 = System.getenv().get("AUTH_TOKEN11");
+		Results results = Runner.path(
+				"src/test/java/org/springframework/api_tests/RoostIoSampleApiRefsHeadsMainApisAddressValidationMqas100Swagger")
+			.systemProperty("API_HOST1", APIHOST1)
+			.systemProperty("AUTH_TOKEN11", AUTHTOKEN11)
+			.reportDir("testReport")
+			.parallel(1);
+		assertEquals(0, results.getFailCount(), results.getErrorMessages());
+	}
+
+}
